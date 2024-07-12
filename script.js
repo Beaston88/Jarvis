b1bf41e2adec90139021e29c51072a1e3acd6973
Ayush Mann <163769727+AyushMannVIT@users.noreply.github.com>
2024-07-12 20:54:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,17 @@ function navAnimation() {
     })
 }
 navAnimation();
+
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+  }
+  function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+  function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+
